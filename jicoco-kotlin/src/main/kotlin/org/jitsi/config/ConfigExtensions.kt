5d5716e693296a6623c664bff0c6d7b2692087e0
bbaldino <bbaldino@gmail.com>
2019-12-05 13:19:08 -0800

    Mask handle null regex (#62)
    * add docs to mask, add unit test
    
    * handle the mask regex field being null

--- jicoco-kotlin/src/main/kotlin/org/jitsi/config/ConfigExtensions.kt
+++ jicoco-kotlin/src/main/kotlin/org/jitsi/config/ConfigExtensions.kt
@@ -1,6 +1,7 @@
 package org.jitsi.config
 
 import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
 import org.jitsi.utils.ConfigUtils
 import java.util.regex.Pattern
@@ -10,12 +11,18 @@ private fun shouldMask(pattern: Pattern?, key: String): Boolean {
     return pattern.matcher(key).find()
 }
 
-private const val MASK = "******"
+const val MASK = "******"
 
+/**
+ * Returns a new [Config] with any values matching
+ * [ConfigUtils.PASSWORD_SYS_PROPS] changed to [MASK].   NOTE
+ * that this will change the type of the value to a [String]
+ */
 fun Config.mask(): Config {
     // Or should this be held in the config itself?
+    val fieldRegex = ConfigUtils.PASSWORD_SYS_PROPS ?: return ConfigFactory.load(this)
     val pattern =
-        Pattern.compile(ConfigUtils.PASSWORD_SYS_PROPS, Pattern.CASE_INSENSITIVE)
+        Pattern.compile(fieldRegex, Pattern.CASE_INSENSITIVE)
     return entrySet().fold(this) { config, (key, _) ->
         when {
             shouldMask(pattern, key) -> config.withValue(key, ConfigValueFactory.fromAnyRef(MASK))
