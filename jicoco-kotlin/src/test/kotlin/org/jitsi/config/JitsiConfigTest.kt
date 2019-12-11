98971fa7c01c12982d891d6baf861fb08240dacd
Brian Baldino <bbaldino@gmail.com>
2019-12-11 12:29:30 -0800

    tweak jitsiconfigtest

--- jicoco-kotlin/src/test/kotlin/org/jitsi/config/JitsiConfigTest.kt
+++ jicoco-kotlin/src/test/kotlin/org/jitsi/config/JitsiConfigTest.kt
@@ -1,6 +1,8 @@
 package org.jitsi.config
 
 import io.kotlintest.IsolationMode
+import io.kotlintest.Spec
+import io.kotlintest.extensions.TopLevelTest
 import org.jitsi.utils.config.ConfigSource
 import org.jitsi.videobridge.testutils.ConfigSourceWrapper
 
@@ -11,6 +13,8 @@ import org.jitsi.videobridge.testutils.ConfigSourceWrapper
  */
 abstract class JitsiConfigTest : ConfigTest() {
     override fun isolationMode(): IsolationMode? = IsolationMode.InstancePerLeaf
+    private val legacyConfigWrapper = ConfigSourceWrapper()
+    private val newConfigWrapper = ConfigSourceWrapper()
 
     override fun withLegacyConfig(configSource: ConfigSource) {
         legacyConfigWrapper.innerConfig = configSource
@@ -20,12 +24,9 @@ abstract class JitsiConfigTest : ConfigTest() {
         newConfigWrapper.innerConfig = configSource
     }
 
-    protected companion object {
-        private val legacyConfigWrapper = ConfigSourceWrapper().also {
-            JitsiConfigFactory.legacyConfigSupplier = { it }
-        }
-        private val newConfigWrapper = ConfigSourceWrapper().also {
-            JitsiConfigFactory.newConfigSupplier = { it }
-        }
+    override fun beforeSpecClass(spec: Spec, tests: List<TopLevelTest>) {
+        super.beforeSpecClass(spec, tests)
+        JitsiConfigFactory.legacyConfigSupplier = { legacyConfigWrapper }
+        JitsiConfigFactory.newConfigSupplier = { newConfigWrapper }
     }
 }
