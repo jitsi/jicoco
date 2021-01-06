e91f23dbf960057d58e499671a0c7a905c10c87d
Boris Grozev <boris@jitsi.org>
2021-01-06 14:38:37 -0600

    ref: Simplify Version, remove suppliers, osgi activator.

--- jicoco/src/test/java/org/jitsi/rest/VersionTest.java
+++ jicoco/src/test/java/org/jitsi/rest/VersionTest.java
@@ -17,45 +17,28 @@
 package org.jitsi.rest;
 
 import org.eclipse.jetty.http.*;
-import org.glassfish.hk2.utilities.binding.*;
 import org.glassfish.jersey.server.*;
 import org.glassfish.jersey.test.*;
 import org.jitsi.utils.version.*;
-import org.jitsi.version.*;
 import org.junit.*;
 
 import javax.ws.rs.core.*;
 
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
-import static org.mockito.Mockito.*;
 
 public class VersionTest extends JerseyTest
 {
-    protected VersionServiceSupplier versionServiceSupplier;
-    protected VersionService versionService;
     protected static final String BASE_URL = "/about/version";
 
     @Override
     protected Application configure()
     {
-        versionServiceSupplier = mock(VersionServiceSupplier.class);
-        versionService = mock(VersionService.class);
-        when(versionServiceSupplier.get()).thenReturn(versionService);
 
         enable(TestProperties.LOG_TRAFFIC);
         enable(TestProperties.DUMP_ENTITY);
         return new ResourceConfig() {
             {
-                register(new AbstractBinder()
-                {
-                    @Override
-                    protected void configure()
-                    {
-                        bind(versionServiceSupplier).to(VersionServiceSupplier.class);
-                    }
-                });
-                register(Version.class);
+                register(new Version(new VersionImpl("appName", 2, 0)));
             }
         };
     }
@@ -63,26 +46,10 @@ public class VersionTest extends JerseyTest
     @Test
     public void testVersion()
     {
-        when(versionService.getCurrentVersion()).thenReturn(
-            new VersionImpl("appName", 2, 0)
-        );
-
         Response resp = target(BASE_URL).request().get();
         assertEquals(HttpStatus.OK_200, resp.getStatus());
         Version.VersionInfo versionInfo = resp.readEntity(Version.VersionInfo.class);
         assertEquals("appName", versionInfo.name);
         assertEquals("2.0", versionInfo.version);
-    }
-
-    @Test
-    public void testNoVersionService()
-    {
-        when(versionServiceSupplier.get()).thenReturn(null);
-        Response resp = target(BASE_URL).request().get();
-        assertEquals(HttpStatus.OK_200, resp.getStatus());
-        Version.VersionInfo versionInfo = resp.readEntity(Version.VersionInfo.class);
-        assertTrue(versionInfo.name.isEmpty());
-        assertTrue(versionInfo.version.isEmpty());
-        assertTrue(versionInfo.os.isEmpty());
     }
 }
