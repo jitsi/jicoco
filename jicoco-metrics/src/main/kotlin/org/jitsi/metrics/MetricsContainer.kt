e703b4f16ef5159341efcbdf9a57333a20284667
Boris Grozev <boris@jitsi.org>
2024-10-14 13:28:07 -0500

    squash: Handle empty as containing */*.

--- jicoco-metrics/src/main/kotlin/org/jitsi/metrics/MetricsContainer.kt
+++ jicoco-metrics/src/main/kotlin/org/jitsi/metrics/MetricsContainer.kt
@@ -79,7 +79,7 @@ open class MetricsContainer @JvmOverloads constructor(
     fun getMetrics(accepts: List<String>): Pair<String, String> {
         return when {
 
-            accepts.contains("*/*") or accepts.contains(OPENMETRICS) ->
+            accepts.isEmpty() or accepts.contains("*/*") or accepts.contains(OPENMETRICS) ->
                 getPrometheusMetrics(TextFormat.CONTENT_TYPE_OPENMETRICS_100) to TextFormat.CONTENT_TYPE_OPENMETRICS_100
             accepts.contains(TEXT) ->
                 getPrometheusMetrics(TextFormat.CONTENT_TYPE_004) to TextFormat.CONTENT_TYPE_004
